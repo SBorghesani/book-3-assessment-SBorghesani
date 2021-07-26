828cc50e61aab1aa869bb262bd8da5b628fb250b
Stefan Borghesani <stefan.borghesani@gmail.com>
2021-07-26 09:30:28 -0500

    applied all the requested changes/debugs as per the assessment requirments

--- scripts/Kids.js
+++ scripts/Kids.js
@@ -1,12 +1,29 @@
 import { getChildren } from "./database.js"
-
 const children = getChildren()
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("child")) {
+            const [, childId] = itemClicked.id.split("--")
+
+            for (const child of children) {
+                if (child.id === parseInt(childId)) {
+                    window.alert(`${child.name}'s wish is ${child.wish}`)
+                }
+            }
+        }
+    }
+)
+
+
 
 export const Kids = () => {
     let html = "<ol>"
 
     for (const child of children) {
-        html += `<li>${child,name}</li>`
+        html += `<li id="child--${child.id}">${child.name}</li>`
     }
 
     html += "</ol>"
--- scripts/Pairings.js
+++ scripts/Pairings.js
@@ -1,16 +1,41 @@
 import { getChildren, getCelebrities } from "./database.js"
-
 const kids = getChildren()
 const celebrities = getCelebrities()
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("kid")) {
+            const [ ,kidId] = itemClicked.id.split("--")
+
+            for (const kid of kids) {
+                if (kid.id === parseInt(kidId)) {
+                    let celebMatch = {}
+
+                    for (const celebrity of celebrities) {
+                        if (celebrity.id === kid.celebrityId) {
+                            celebMatch = celebrity
+                            window.alert(`${kid.name} will be making memories with ${celebMatch.name}, a ${celebMatch.sport} star, by ${kid.wish}
+                            `)
+                        }
+                    }
+                }
+            }
+        }
+    }
+)
+
 
 const findCelebrityMatch = (kidObject, celebrityArray) => {
-    let celebrity = null
-
+    
+    let celebrityMatch = "null"
     for (const celebrity of celebrityArray) {
-
+        if (celebrity.id === kidObject.celebrityId) {
+           celebrityMatch = celebrity
+        }
     }
+    return celebrityMatch
 
-    return celebritiy
 }
 
 export const Pairings = () => {
@@ -18,10 +43,11 @@ export const Pairings = () => {
     html = "<ul>"
 
     for (const kid of kids) {
+        
         const kidsStar = findCelebrityMatch(kid, celebrities)
         html += `
-            <li>
-                ${kid.name} will be making memories with ${kidsStar.name}, a ${kidsStar.Sport} star, by ${kid.wish}
+            <li id="kid--${kid.id}">
+                ${kid.name} will be making memories with ${kidsStar.name}, a ${kidsStar.sport} star, by ${kid.wish}
             </li>
         `
     }
